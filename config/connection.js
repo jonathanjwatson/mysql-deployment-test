9a0193404bfd4f5e9cc9d4e7a3524a2d07bc7e3f
jwats287 <jwats287@users.noreply.github.com>
2019-09-07 09:42:06 -0400

    modified to accept jawsdb_url

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.PASSWORD,
-  database: "test_db"
-});
+var connection;
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.PASSWORD,
+        database: "test_db"
+      });
+};
 
 // Make connection.
 connection.connect(function(err) {
